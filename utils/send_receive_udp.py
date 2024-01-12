cac8311d34e497c30196ad7dcae78ae87213aa95
Helmut M <hmedchen@gmail.com>
2024-01-12 08:34:57 +0100

    DISAG broadcast dummy update

--- utils/send_receive_udp.py
+++ utils/send_receive_udp.py
@@ -5,6 +5,7 @@ import json
 
 #UDP_IP = "192.168.0.137"
 UDP_IP = "127.0.0.1"
+#UDP_IP = "1.1.1.1"
 # DISAG port: 30169
 UDP_PORT = 30169
 MESSAGE = b"Hello, World!"
@@ -19,7 +20,14 @@ def send():
     # send
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Internet  # UDP
     print(f"sending {MESSAGE}")
-    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+   # sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+    while True:
+        with open("log.json","r") as fp:
+            lines = fp.readlines()
+            for line in lines:
+                print(f"{line} ({len(line)})chars")
+                sock.sendto(line.encode('utf-8'), (UDP_IP, UDP_PORT))
+                time.sleep(2)
 
 
 
@@ -33,10 +41,13 @@ def receive():
     while True:
         data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
         try:
+            print(type(data))
             jsondata = json.loads(data)
-            with open("log.json","a") as fp:
-                fp.write(str(data))
-            print(f"received message: \n {json.dumps(jsondata)}")
+            with open("log2.json","a") as fp:
+                json.dump(data.decode('utf-8'), fp)
+                fp.write("\n")
+#                fp.write(data))
+            print(f"received message: \n {json.dumps(jsondata, indent=2)}")
         except:
             print("received message: %s" % data)
             raise
